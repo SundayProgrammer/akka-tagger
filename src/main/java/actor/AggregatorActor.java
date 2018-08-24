5ebb2790eb97831ac33081c45d332feaa1ed7cc0
SundayProgrammer <mat357@o2.pl>
2018-08-24 11:42:09 +0200

    Words extraction in WrapperActor

--- src/main/java/actor/AggregatorActor.java
+++ src/main/java/actor/AggregatorActor.java
@@ -2,8 +2,19 @@ package actor;
 
 import akka.actor.AbstractActor;
 
+import java.util.List;
+
 public class AggregatorActor extends AbstractActor {
 
+    static public class Words {
+        public final List extractedWords;
+
+        public Words(List extractedWords) {
+            this.extractedWords = extractedWords;
+        }
+    }
+
+    @Override
     public Receive createReceive() {
         return null;
     }
--- src/main/java/actor/WrapperActor.java
+++ src/main/java/actor/WrapperActor.java
@@ -1,11 +1,46 @@
 package actor;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+
+import actor.AggregatorActor.Words;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class WrapperActor extends AbstractActor {
 
+    static public Props props(final String message, final ActorRef aggregatorActor) {
+        return Props.create(WrapperActor.class, () -> new WrapperActor(message, aggregatorActor));
+    }
 
+    private final ActorRef aggregatorActor;
+    private final String message;
+    private List extractedWords;
+
+    public WrapperActor(String message, ActorRef aggregatorActor) {
+        this.message = message;
+        this.aggregatorActor = aggregatorActor;
+    }
+
+    static public class ExtractWords {
+        public ExtractWords() {}
+    }
+
+    static public class Resolve {
+        public Resolve() {}
+    }
+
+    @Override
     public Receive createReceive() {
-        return null;
+        return receiveBuilder()
+                .match(ExtractWords.class, x -> {
+                    extractedWords = Arrays.asList(this.message.split(" "));
+                })
+                .match(Resolve.class, x -> {
+                    aggregatorActor.tell(new Words(extractedWords), getSelf());
+                })
+                .build();
     }
 }
