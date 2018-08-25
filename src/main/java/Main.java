f554201e2bbc46862772b3bfa2252118a1848349
SundayProgrammer <mat357@o2.pl>
2018-08-25 10:41:52 +0200

    Added input from file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,13 +5,17 @@ import actor.WrapperActor.ExtractWords;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import helpers.Categorization;
+import org.apache.commons.lang3.math.NumberUtils;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static config.AppVars.RULES_PATH;
+import static config.AppVars.SENTENCES_PATH;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,10 +24,12 @@ public class Main {
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         Categorization categorization = new Categorization();
+        ArrayList<String> sentences;
         try {
             categorization.readRules(RULES_PATH);
+            sentences = categorization.readSentences(SENTENCES_PATH);
         } catch (FileNotFoundException e) {
-            System.out.println("Rules not found");
+            System.out.println("Rules or sentences not found /n" + e);
             return;
         }
         Boolean order = true;
@@ -38,30 +44,28 @@ public class Main {
 
         while (order) {
             String input = "";
-            System.out.println("c - check sentence");
-            System.out.println("q - quit");
+            int num = 0;
+            System.out.println("Give queries number: ");
             try {
                 input = bufferedReader.readLine();
+                if (NumberUtils.isCreatable(input)) {
+                    num = Integer.parseInt(input);
+                } else {
+                    continue;
+                }
             } catch (IOException e) {
+                e.printStackTrace();
+            } catch (NumberFormatException e) {
                 e.printStackTrace();
             }
 
-            switch (input.charAt(0)) {
-                case 'c':
-                    String sentence;
-                    try {
-                        sentence = bufferedReader.readLine();
-                        wrapperActor.tell(new ExtractWords(sentence), ActorRef.noSender());
-                        wrapperActor.tell(new Resolve(), ActorRef.noSender());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                case 'q':
-                    order = false;
-                    break;
-                default:
-                    System.out.println("Wrong input");
+            int aviableSentences = sentences.size();
+            int random = 0;
+
+            for (int i = num; i > 0; i--) {
+                random = ThreadLocalRandom.current().nextInt(0, aviableSentences);
+                wrapperActor.tell(new ExtractWords(sentences.get(random)), ActorRef.noSender());
+                wrapperActor.tell(new Resolve(), ActorRef.noSender());
             }
         }
     }
--- src/main/java/helpers/Categorization.java
+++ src/main/java/helpers/Categorization.java
@@ -3,8 +3,10 @@ package helpers;
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -24,6 +26,23 @@ public class Categorization {
         JsonReader reader = new JsonReader(new FileReader(path));
         Rule[] rulesArray = gson.fromJson(reader, Rule[].class);
         this.rules = new ArrayList<Rule>(Arrays.asList(rulesArray));
+    }
+
+    public ArrayList<String> readSentences(String path) throws FileNotFoundException {
+
+        ArrayList<String> sentences = new ArrayList<>();
+
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        try {
+            String sentence;
+            while ((sentence = br.readLine()) != null)  {
+                sentences.add(sentence);
+            }
+        } catch (IOException ioe) {
+            System.out.println("IOException: " + ioe);
+        }
+
+        return sentences;
     }
 }
 
